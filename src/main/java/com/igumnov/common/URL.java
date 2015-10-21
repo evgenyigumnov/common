1d35d0c08011553778d1c3b2b7fe2126f05e0371
igumnov <igumnov@gmail.com>
2015-10-21 16:28:31 +0600

    URL timing

--- src/main/java/com/igumnov/common/URL.java
+++ src/main/java/com/igumnov/common/URL.java
@@ -26,10 +26,10 @@ public class URL {
 
         java.net.URL urlObj = new java.net.URL(url);
         StringBuilder ret = new StringBuilder();
-        try ( InputStreamReader stream = new InputStreamReader( urlObj.openStream(), "UTF-8" ) ) {
-            BufferedReader reader = new BufferedReader( stream );
-            for ( int c = reader.read(); c != -1; c = reader.read() ) {
-                ret.append( ( char ) c );
+        try (InputStreamReader stream = new InputStreamReader(urlObj.openStream(), "UTF-8")) {
+            BufferedReader reader = new BufferedReader(stream);
+            for (int c = reader.read(); c != -1; c = reader.read()) {
+                ret.append((char) c);
             }
 
         }
@@ -38,55 +38,58 @@ public class URL {
     }
 
     public static String getAllToString(String url, String method, Map<String, Object> postParams, String postBody) throws IOException {
-        java.net.URL u = new java.net.URL(url);
-
-        StringBuilder postData = new StringBuilder();
-        if (postParams != null) {
-            for (Map.Entry<String, Object> param : postParams.entrySet()) {
-                if (postData.length() != 0) postData.append('&');
-                postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
-                postData.append('=');
-                postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
-            }
-        }
-
-        if (postBody != null) {
-            postData.append(postBody);
-        }
-
-        byte[] postDataBytes = postData.toString().getBytes("UTF-8");
-        HttpURLConnection conn = null;
-        String timerName = "url" + Number.randomIntByRange(11111111,999999999);
-        try {
-            Benchmark.timerStart(timerName);
-            conn = (HttpURLConnection) u.openConnection();
-            Log.debug("Timing url open connect " + Benchmark.timerStop(timerName) + " " + url);
-        } catch (TimeException e) {
-            e.printStackTrace();
-        }
-        conn.setRequestMethod(method);
-        conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
-        conn.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
-        conn.setDoOutput(true);
-        conn.getOutputStream().write(postDataBytes);
-        Reader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
         StringBuilder ret = new StringBuilder();
-        for (int c = in.read(); c != -1; c = in.read()) {
-            ret.append((char) c);
-        }
+        try {
+            String timerName = "url" + Number.randomIntByRange(11111111, 999999999);
+            Benchmark.timerStart(timerName);
 
-        conn.disconnect();
+            java.net.URL u = new java.net.URL(url);
+
+            StringBuilder postData = new StringBuilder();
+            if (postParams != null) {
+                for (Map.Entry<String, Object> param : postParams.entrySet()) {
+                    if (postData.length() != 0) postData.append('&');
+                    postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+                    postData.append('=');
+                    postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+                }
+            }
+
+            if (postBody != null) {
+                postData.append(postBody);
+            }
+
+            byte[] postDataBytes = postData.toString().getBytes("UTF-8");
+            HttpURLConnection conn = null;
+            conn = (HttpURLConnection) u.openConnection();
+            conn.setRequestMethod(method);
+            conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+            conn.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
+            conn.setDoOutput(true);
+            conn.getOutputStream().write(postDataBytes);
+            Reader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+            for (int c = in.read(); c != -1; c = in.read()) {
+                ret.append((char) c);
+            }
+
+            conn.disconnect();
+            Log.debug("Timing url " + Benchmark.timerStop(timerName) + " " + url);
+        } catch (Exception e) {
+            throw new IOException(e);
+        }
         return ret.toString();
     }
 
     public static void turnOffCertificateValidation() throws NoSuchAlgorithmException, KeyManagementException {
         // Create a trust manager that does not validate certificate chains
-        TrustManager[] trustAllCerts = new TrustManager[] {new X509TrustManager() {
+        TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
             public java.security.cert.X509Certificate[] getAcceptedIssuers() {
                 return null;
             }
+
             public void checkClientTrusted(X509Certificate[] certs, String authType) {
             }
+
             public void checkServerTrusted(X509Certificate[] certs, String authType) {
             }
         }
